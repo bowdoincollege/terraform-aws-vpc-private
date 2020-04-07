922c35312215531acce1b864111ab9a53b0c2ca4
Jason Lavoie <jlavoie@bowdoin.edu>
2020-04-07 08:57:42 -0400

    add key and sg

--- instance.tf
+++ instance.tf
@@ -4,7 +4,20 @@ data "aws_ami" "amazon_linux_2" {
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm*"]
+    values = ["amzn2-ami-hvm*x86_64-ebs"]
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.this.id
+
+  ingress {
+    cidr_blocks = ["139.140.0.0/16"]
+    from_port   = 0
+    to_port     = 22
+    protocol    = "tcp"
   }
 }
 
@@ -13,4 +26,7 @@ resource "aws_instance" "test" {
   associate_public_ip_address = true
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.private["a"].id
+  vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
+  # tflint-ignore: aws_instance_invalid_key_name
+  key_name = "vpgtest1"
 }
